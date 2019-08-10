d6fe56bd3a89c4d906a524f57e0100f6cf70f0e2
PavelNestor <pavelnesterets82@gmail.com>
2019-08-10 11:18:09 +0300

    add authorization

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
 
-const HomePage = () => <h1>HomePage</h1>;
-export default HomePage;
+import { withAuthorization } from '../Session';
+
+const HomePage = () => (
+  <div>
+    <h1>Home Page</h1>
+    <p>The Home Page is accessible by every signed in user.</p>
+  </div>
+);
+
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(HomePage);
